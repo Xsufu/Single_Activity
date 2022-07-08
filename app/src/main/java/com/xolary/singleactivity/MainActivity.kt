7d1fe75355923286b689033e0c74a91e80819726
Xsufu <aluno.macarrao@gmail.com>
2022-07-09 04:39:03 +0700

    Перевод на data binding и добавление навигации

--- app/src/main/java/com/xolary/singleactivity/MainActivity.kt
+++ app/src/main/java/com/xolary/singleactivity/MainActivity.kt
@@ -2,10 +2,34 @@ package com.xolary.singleactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.xolary.singleactivity.databinding.ActivityMainBinding
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        // Set up the action bar for use with the NavController
+        setupActionBarWithNavController(this, navController)
+    }
+
+    /**
+     * Handle navigation when the user chooses Up from the action bar.
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
