9fbfff9e6be178d52d4b6315f5756611e7c56d14
Xsufu <aluno.macarrao@gmail.com>
2022-07-09 04:38:22 +0700

    Создан список контактов

---
+++ app/src/main/java/com/xolary/singleactivity/data/DataSource.kt
@@ -0,0 +1,33 @@
+package com.xolary.singleactivity.data
+
+import com.xolary.singleactivity.R
+
+object DataSource {
+    val contacts: List<Contact> = listOf(
+        Contact(
+            "Anton",
+            "Kosovskiy",
+            R.drawable.avatar_sample
+        ),
+        Contact(
+            "Ivan",
+            "Gorshkov",
+            R.drawable.avatar_sample
+        ),
+        Contact(
+            "Nikita",
+            "Bergen",
+            R.drawable.avatar_sample
+        ),
+        Contact(
+            "Denis",
+            "Zyrianov",
+            R.drawable.avatar_sample
+        ),
+        Contact(
+            "Valeria",
+            "Shulga",
+            R.drawable.avatar_sample
+        )
+    )
+}
