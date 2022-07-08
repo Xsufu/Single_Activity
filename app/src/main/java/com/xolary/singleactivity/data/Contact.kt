37757d4132e6c570fe0d707250cc5d0f108996b3
Xsufu <aluno.macarrao@gmail.com>
2022-07-09 04:37:47 +0700

    Добавлена аннотация к переменной ID аватара

--- app/src/main/java/com/xolary/singleactivity/data/Contact.kt
+++ app/src/main/java/com/xolary/singleactivity/data/Contact.kt
@@ -1,7 +1,9 @@
 package com.xolary.singleactivity.data
 
+import androidx.annotation.DrawableRes
+
 data class Contact(
     val firstName: String,
     val lastName: String,
-    val avatarResourceId: Int
+    @DrawableRes val avatarResourceId: Int
 )
