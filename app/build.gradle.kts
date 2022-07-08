12f1a9c106661b012ef6f628f0d96239a4d41728
Xsufu <aluno.macarrao@gmail.com>
2022-07-08 14:21:42 +0700

    Включён data binding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,10 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures {
+        dataBinding = true
+    }
+
     buildTypes {
         getByName("release") {
             isMinifyEnabled = true
@@ -43,4 +47,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+
+    // Jetpack
+    val lifecycle_version = "2.2.0"
+
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
 }
