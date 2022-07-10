96db552f39120801a9e508036350d2060457b6c5
Xsufu <aluno.macarrao@gmail.com>
2022-07-10 08:40:34 +0700

    Установка значений в поля вынесена в функцию внутри ViewHolder класса, в конструктор класса добавлена переменная класса для прослушки нажатий,установлена прослушка нажатий на элемент списка

--- app/src/main/java/com/xolary/singleactivity/adapter/ContactCardAdapter.kt
+++ app/src/main/java/com/xolary/singleactivity/adapter/ContactCardAdapter.kt
@@ -7,30 +7,46 @@ import com.xolary.singleactivity.data.Contact
 import com.xolary.singleactivity.data.DataSource
 import com.xolary.singleactivity.databinding.ContactsListContactBinding
 
-class ContactCardAdapter(): RecyclerView.Adapter<ContactCardAdapter.ContactCardViewHolder>() {
+
+class ContactCardAdapter(private val onClickLister: OnClickListener): RecyclerView.Adapter<ContactCardAdapter.ContactCardViewHolder>() {
 
     private val dataset = DataSource.contacts
 
-    class ContactCardViewHolder(var binding: ContactsListContactBinding):
-        RecyclerView.ViewHolder(binding.root) {
-    }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactCardViewHolder {
 
-        val binding = ContactsListContactBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ContactCardViewHolder(binding)
+        return ContactCardViewHolder(
+            ContactsListContactBinding.inflate(
+                LayoutInflater.from(
+                    parent.context
+                )
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: ContactCardViewHolder, position: Int) {
         val item = dataset[position]
-
-        holder.binding.contactName.text = item.firstName + " " + item.lastName
-        holder.binding.contactPhoto.setImageResource(item.avatarResourceId)
-
-        holder.itemView.setOnClickListener {
-            // TODO: Написать действие при нажатии на элемент списка
+        holder.bind(item)
+        val fullName = item.firstName + " " + item.lastName
+        holder.itemView.setOnClickListener{
+            onClickLister.clickListener(item, fullName)
         }
     }
 
+    class ContactCardViewHolder(private var binding: ContactsListContactBinding):
+        RecyclerView.ViewHolder(binding.root) {
+
+            fun bind(item: Contact) {
+                val fullName = item.firstName + " " + item.lastName
+                binding.apply {
+                    contactName.text = fullName
+                    contactPhoto.setImageResource(item.avatarResourceId)
+                }
+            }
+    }
+
     override fun getItemCount() = dataset.size
+
+    class OnClickListener(val clickListener: (contact: Contact, fullName: String) -> Unit) {
+        fun onClick(contact: Contact, fullName: String) = clickListener(contact, fullName)
+    }
 }
