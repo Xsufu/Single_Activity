96a10f93a9a6a983fd140fe16f7cfe9c010bd39f
Xsufu <aluno.macarrao@gmail.com>
2022-07-10 08:45:55 +0700

    Создана переменная для функции, запускающей навигацию по клику на элемент списка

--- app/src/main/java/com/xolary/singleactivity/ContactsListFragment.kt
+++ app/src/main/java/com/xolary/singleactivity/ContactsListFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.xolary.singleactivity.adapter.ContactCardAdapter
@@ -32,10 +33,13 @@ class ContactsListFragment : Fragment() {
     }
 
     private fun updateUI() {
-//        val contacts = DataSource.contacts
-        adapter = ContactCardAdapter()
+        adapter = ContactCardAdapter(listener)
         contactsRecyclerView.adapter = adapter
     }
 
-
+    private val listener = ContactCardAdapter.OnClickListener { contact, fullName ->
+        findNavController().navigate(
+            ContactsListFragmentDirections.actionContactsListFragmentToContactDetails(contact, fullName)
+        )
+    }
 }
