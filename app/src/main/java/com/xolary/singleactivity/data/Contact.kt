03425f98fa4d7a0f3193a8a4991f2b238b766292
Xsufu <aluno.macarrao@gmail.com>
2022-07-10 08:34:38 +0700

    Класс переведён в Parcelable

--- app/src/main/java/com/xolary/singleactivity/data/Contact.kt
+++ app/src/main/java/com/xolary/singleactivity/data/Contact.kt
@@ -1,9 +1,12 @@
 package com.xolary.singleactivity.data
 
+import android.os.Parcelable
 import androidx.annotation.DrawableRes
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Contact(
     val firstName: String,
     val lastName: String,
     @DrawableRes val avatarResourceId: Int
-)
+): Parcelable
